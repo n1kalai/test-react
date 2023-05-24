08c00a015e634e7a5dee275171ca04e0c51123f2
tengo minasovi <115106443+Tengominasovi@users.noreply.github.com>
2023-05-24 19:08:23 +0400

    works

--- src/api/cocktails.js
+++
@@ -1,7 +0,0 @@
-export const fetchCocktails = async() => {
-	return fetch(
-		"https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
-	)
-		.then((res) => res.json())
-		.then((data) => data.drinks);
-};
