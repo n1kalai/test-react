7999f2edb937a2a9c3a05c78a3141aee6b447bd7
buquqa <malxazibarnabishvili@gmail.com>
2023-05-28 23:15:21 +0400

    did todos homework

--- src/App.js
+++ src/App.js
@@ -2,69 +2,66 @@ import "./App.css";
 import { useState } from "react";
 
 const App = () => {
-	const [user, setUser] = useState({
-		first_name: "",
-		last_name: "",
-		number: 1231231,
-		sex: "",
-	});
+  const [toDo, setTodo] = useState({
+    text: "",
+  });
+  const [todos, setTodos] = useState([]);
 
-	const handleInputChange = (event) => {
-		const { name, value } = event.target;
+  const handleChangevalue = (val) => {
+    setTodo((prev) => {
+      return { ...prev, [val.name]: val.value };
+    });
+  };
 
-		setUser((prevState) => ({
-			...prevState,
-			[name]: value,
-		}));
-	};
+  const handleAddToDo = () => {
+    const newTodo = {
+      ...toDo,
+      id: Math.floor(Math.random() * 1000),
+    };
+    setTodos((prev) => [...prev, newTodo]);
+    setTodo({ text: "" });
+  };
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		console.log(user);
-	};
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<legend>registration</legend>
-			<input
-				placeholder="name"
-				name="first_name"
-				value={user.first_name}
-				onChange={handleInputChange}
-			/>
-			<input
-				onChange={handleInputChange}
-				placeholder="last name"
-				name="last_name"
-				value={user.last_name}
-			/>
-			<div>
-				<div>your sex</div>
-				<div>male</div>
-				<input
-					type="radio"
-					onChange={handleInputChange}
-					value="male"
-					name="sex"
-				/>
-				<div>female</div>
-				<input
-					type="radio"
-					onChange={handleInputChange}
-					value="female"
-					name="sex"
-				/>
-				<div>other</div>
-				<input
-					type="radio"
-					onChange={handleInputChange}
-					value="other"
-					name="sex"
-				/>
-			</div>
-			<button>submit</button>
-		</form>
-	);
+  const handleDeleteTodo = (index) => {
+    console.log(index);
+    const upDatedToDos = [...todos];
+    upDatedToDos.splice(index, 1);
+    setTodos(upDatedToDos);
+  };
+
+  return (
+    <div className="Main-Div">
+      <h1 className="Headone">TODO LIST</h1>
+      <hr className="hr"></hr>
+      <form onSubmit={handleSubmit} className="form">
+        <input
+          onChange={(event) => handleChangevalue(event.target)}
+          value={toDo.text}
+          name="text"
+          className="input"
+          placeholder="add item..."
+        />
+        <button onClick={handleAddToDo} className="button">
+          ADD
+        </button>
+      </form>
+      <ul className="return">
+        {todos.map((todo, index) => (
+          <li
+            className="li"
+            onClick={() => handleDeleteTodo(index)}
+            key={todo.id}
+          >
+            {todo.text}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
