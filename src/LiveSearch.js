d3f28bfe3fc13b9d1b81b339f95e01e9251cc58a
ninigibradze <ninigibradze@gmail.com>
2023-05-28 09:48:29 +0400

    created livesearch js

---
+++ src/LiveSearch.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
+
+const LiveSearch = () => {
+	
+	return (
+		<div>
+            
+        </div>
+	);
+};
+
+export default LiveSearch;
+
