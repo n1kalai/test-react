8af0f30b6638f645f5587fda94ce00fd399deba5
Nikoloz <nikoloz.palagashvili@gmail.com>
2023-05-31 21:59:20 +0400

    added quiz

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import AppWithClass from "./AppWithClass";
 import LiveSearch from "./LiveSearch";
+import Quiz from "./components/quiz/Quiz";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<LiveSearch />
+		<Quiz />
 	</React.StrictMode>
 );
 
