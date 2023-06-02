0244cb2b3b138caa5c12723d73c4198e4b951488
DatoGGT <dato.gogotishvilii@gmail.com>
2023-06-03 01:05:05 +0400

    slider-dg

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import AppWithClass from "./AppWithClass";
+import Slider from "./components/Slider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-	<App />
-	// </React.StrictMode>
+	<Slider/>
+	 </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
