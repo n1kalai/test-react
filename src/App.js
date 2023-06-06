abc6231217ec3c730c90ef9fd10e1c463b24a32f
ninigibradze <ninigibradze@gmail.com>
2023-06-06 15:53:39 +0400

    did hook homework with 3 buttons to show round/odd id cocktails and to return back to original array

--- src/App.js
+++ src/App.js
@@ -1,48 +1,157 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { Languages } from "./components/Languages";
-import { useState, useEffect } from "react";
-import { fetchCocktails } from "./api/cocktails";
+import React, { useEffect, useState } from "react";
+import { useFetchCocktails } from "./api/cocktails";
+import CocktailContainer from "./components/cocktails/CocktailContainer";
 
-const cocktailsDefaultStatee = {
+const defaultCocktails = {
 	data: [],
 	isLoading: true,
 	isLoaded: false,
 	isError: false,
-};
+}
 
 const App = () => {
-	const [number, setNumber] = useState(1);
-	const [name, setName] = useState("gela");
-	const [cocktails, setCocktails] = useState(cocktailsDefaultStatee);
-	console.log(cocktails);
-	useEffect(() => {
-		handleFetchCocktails();
-	}, []);
+	const [cocktails, setCocktails] = useState(defaultCocktails);
 
-	const handleFetchCocktails = async () => {
-		const fetchedCocktails = await fetchCocktails();
+	const cocktailsArray = useFetchCocktails();
+
+	useEffect(() => {
 		setCocktails({
-			data: fetchedCocktails,
+			data: cocktailsArray,
+			isLoading: false,
+			isLoaded: true,
+			isError: false,
+		})
+	}, [cocktailsArray]);
+	
+
+
+	const handleDelete = (id) => {
+		const filteredArr = cocktails.data.filter(
+			(cocktail) => cocktail.idDrink !== id
+		);
+		setCocktails({
+			data: filteredArr,
 			isLoading: false,
 			isLoaded: true,
 			isError: false,
 		});
 	};
 
-	const onClick = () => {
-		setName(Math.random());
-	};
+	const showOddNumId = () => {
+		const newOddNumArray = cocktailsArray.filter((element) => element.idDrink % 2 === 1);
+		setCocktails({
+			data: newOddNumArray,
+			isLoading: false,
+			isLoaded: true,
+			isError: false,
+		})
+	}
+
+	const showRoundNumId = () => {
+		const newRoundNumArray = cocktailsArray.filter((element) => element.idDrink % 2 === 0);
+		setCocktails({
+			data: newRoundNumArray,
+			isLoading: false,
+			isLoaded: true,
+			isError: false,
+		})
+	}
+
+	const showOriginalData = () => {
+		setCocktails({
+			data: cocktailsArray,
+			isLoading: false,
+			isLoaded: true,
+			isError: false,
+		})
+	}
 
 	if (cocktails.isLoading) {
-		return <h1>იტვირთებაa...</h1>;
+		return <h1>Loading...</h1>;
+	}
+
+	if (cocktails.isError) {
+		return <h1>error</h1>;
+	}
+
+	if (!cocktails.data.length) {
+		return <h1>No cocktails found</h1>;
 	}
 
 	return (
-		<button onClick={onClick}>
-			{number} {name}
-		</button>
+		<div  className="main_cont">			
+			<section className="cocktails-container">
+				{cocktails.data.map((cocktail) => (
+					<CocktailContainer
+						onDelete={handleDelete}
+						key={cocktail.idDrink}
+						cocktail={cocktail}
+					/>
+			))}
+			</section>
+			<div className="right_cont">
+				<p className="data_length">{cocktails.data.length}</p>
+ 				<button className="odd_but" onClick={showOddNumId}>Odd numbere ID</button>
+				<button className="odd_but" onClick={showRoundNumId}>Round numbere ID</button>
+				<button className="odd_but" onClick={showOriginalData}>Show all Cocktails</button>
+			</div>
+		</div>
 	);
 };
 
 export default App;
+
+
+
+
+// import { useState } from "react";
+// import { useFetchCocktails } from "./api/cocktails";
+// import CocktailContainer from "./components/cocktails/CocktailContainer";
+
+// const App = () => {	
+// 	const [cocktails, setCocktails] = useState({
+// 		data: [],
+// 		isLoading: true,
+// 		isLoaded: false,
+// 		isError: false,}); 
+
+// 	const newCocktailsArr = useFetchCocktails();
+// 	console.log(newCocktailsArr)
+// 	setCocktails({data: newCocktailsArr,
+// 		isLoading: true,
+// 		isLoaded: false,
+// 		isError: false,});
+
+// 	const handleDelete = () => {
+
+// 	}
+
+// 	const showOddNumId = () => {
+// 	}
+
+
+// 	if (!cocktails.data.length) {
+// 		return <h1>No cocktails found</h1>;
+// 	}
+
+// 	// const newArray = cocktails.data.filter((element) => element.idDrink % 2 === 0);
+
+	
+
+// 	return (
+// 		<div className="main_cont">
+// 			<section className="cocktails-container">
+// 			{cocktails.data.map((cocktail) => (
+// 					<CocktailContainer
+// 						onDelete={handleDelete}
+// 						key={cocktail.idDrink}
+// 						cocktail={cocktail}
+// 					/>
+// 				))}				
+// 			</section>
+// 			<div>
+// 				<p className="data_length">{cocktails.data.length}</p>
+// 				<button className="odd_but" onClick={showOddNumId}>Odd numbere ID</button>
+// 			</div>
+// 		</div>
+// 	);
--- src/api/cocktails.js
+++ src/api/cocktails.js
@@ -1,15 +1,24 @@
-export const fetchCocktails = async () => {
-	const cocktails = await fetch(
-		"https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
-	);
-	const jsonData = await cocktails.json();
-	return jsonData.drinks;
-};
+import { useEffect, useState } from "react";
 
-// export const fetchCocktails = () => {
-// 	return fetch(
-// 		"https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
-// 	)
-// 		.then((res) => res.json())
-// 		.then((data) => data.drinks);
-// };
+export const useFetchCocktails = () => {
+	const [cocktails, setCocktails] = useState([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch(
+					"https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+				);
+				const jsonData = await response.json();
+				setCocktails(jsonData.drinks);
+			} catch (error) {
+				console.error("Error fetching cocktails:", error);
+				setCocktails([]);
+			}
+		};
+
+		fetchData();
+	}, []);
+
+	return cocktails;
+};
