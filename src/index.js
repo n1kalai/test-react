053ea4bafdedb9403b695417af996ca7dacae100
buquqa <malxazibarnabishvili@gmail.com>
2023-06-09 17:14:47 +0400

    did reduces task 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
-
 import reportWebVitals from "./reportWebVitals";
-
-import Hooks from "./components/hooks/Hooks";
-
+import CountReduse from "./reduce";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	// <React.StrictMode>
-		<Hooks />
-	// </React.StrictMode>
+  // <React.StrictMode>
+  <CountReduse />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
