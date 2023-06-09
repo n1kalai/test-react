534d07209fd2ee2f32b4d6cb94e7ff3521702d9f
DatoGGT <dato.gogotishvilii@gmail.com>
2023-06-09 13:30:45 +0400

    reducerTimer-DG

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import reportWebVitals from "./reportWebVitals";
-import CostumHookDG from "./components/hooks/CostumHookDG";
-import LoadingHook from "./components/LoadingHook";
-import MapOdd from "./components/MapOdd";
-import Lenght from "./components/hooks/Lenght";
+import Reducer from "./components/hooks/Reducer";
+
+
+
 
 
 
@@ -13,13 +13,13 @@ import Lenght from "./components/hooks/Lenght";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+
 	<React.StrictMode>
-		<CostumHookDG />
-		<LoadingHook/>
-		<MapOdd/>
-		<Lenght/>
+	<Reducer/>
 		
 	</React.StrictMode>
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
