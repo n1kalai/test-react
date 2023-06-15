5971dbe8f5f33cd4dd6776aea22a9fdc4ede2d9a
ninigibradze <ninigibradze@gmail.com>
2023-06-15 18:06:11 +0400

    tried material ui elements

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { Footer } from "./components/Footer";
 import { About } from "./components/About";
 import "./index.css";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	// <React.StrictMode>
