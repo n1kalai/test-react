a60bd4725c19b665387d297fe1560d8cae3f4ed2
buquqa <malxazibarnabishvili@gmail.com>
2023-06-19 15:17:28 +0400

    did react router hw 39 B-B

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import ComponentA from "./contexts/componentA";
-import "./contexts/context.css";
-
+import MainPage from "./mainPage";
+import SecondPage from "./secondPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <ComponentA />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/secondPage/:id" element={<SecondPage />} />
+    </Routes>
+  </BrowserRouter>
   // </React.StrictMode>
 );
 
