a07347d9660271dfdb318e1d957d5639ae1ec360
ninigibradze <ninigibradze@gmail.com>
2023-06-20 17:35:33 +0400

    removed unnecessary folder

--- src/api/fetchPosts.js
+++
@@ -1,7 +0,0 @@
-const fetchingPosts = async () => {
-	const posts = await fetch("https://jsonplaceholder.typicode.com/posts");
-	const jsonData = await posts.json();
-	return jsonData;
-};
-
-export default fetchingPosts;
